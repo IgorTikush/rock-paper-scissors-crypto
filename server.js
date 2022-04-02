c9102d98c22380787344b6fc54a01b2a07de7c18
IgorTikush <igortikush@mail.ru>
2022-04-02 23:36:40 +0800

    test

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const path = require('path');
 const {readdirSync} = require('fs');
 
-const publicDirectory = path.join(__dirname, '../dist');
+const publicDirectory = path.join(__dirname, 'dist');
 
 const app = express();
 app.use(express.static(publicDirectory));
 app.get('/', function (req, res) {
   console.log(readdirSync(path.join(__dirname, 'dist')));
-  res.sendFile(path.join(publicDirectory, "index.html"));
+  res.sendFile(path.join(publicDirectory, 'index.html'));
 })
 
 
